51d60a8facbb2e5cb9cf04a0f287fc8ae2c84ec1
Jettsyy <teazyyy2512@gmail.com>
2025-04-02 16:05:06 +0700

    feat: read and delete product

--- add.js
+++ add.js
@@ -2,21 +2,26 @@
 
 
 const handleSubmit = async (event) => {
+    // Ngăn sự kiện mặc định
     event.preventDefault();
+    // lấy dữ liệu từ các ô input thông qua id
     const title = document.getElementById("title").value;
     const image = document.getElementById("image").value;
     const price = document.getElementById("price").value;
     const category = document.getElementById("category").value;
 
+    // validation
     if(!title ||!image || !price || !category){
         return alert("Vui long nhap day du thong tin")
     }
+    // gộp dữ liệu thành 1 đối tượng payload
     const payload = {
         title,
         image,
         price: Number(price),
         category
     }
+    // Call api đẩy dữ liệu lên server
     try {
         await axios.post('http://localhost:3000/products',payload)
         location.href= "/index.html"
---
+++ index.js
@@ -0,0 +1,49 @@
+const renderRow = (
+  product = {
+    id: "",
+    title: "",
+    image: "",
+    price: "",
+    category: "",
+  }
+) => {
+  return `
+    <tr>
+        <td>${product.id}</td>
+        <td>${product.title}</td>
+        <td><img src="${product.image}" width="100px"></td>
+        <td>${product.price}</td>
+        <td>${product.category}</td>
+        <td>
+        <button class="btn btn-danger" onClick="deleteProduct(${product.id})"> Delete</button>
+        <button class="btn btn-primary"> Update</button>
+        </td>
+    </tr>
+    `;
+};
+
+//khai báo
+const getProduct = async () => {
+  try {
+    const { data } = await axios.get("http://localhost:3000/products");
+    const htmlRender = data.map(renderRow).join("");
+    document.getElementById("listProduct").innerHTML = htmlRender;
+  } catch (error) {
+    console.log(error);
+  }
+};
+// Gọi hàm
+getProduct();
+
+const deleteProduct = async (id) => {
+  const isConfirm = confirm("Ban co chac muon xoa khong");
+  if (!isConfirm) {
+    return;
+  }
+  try {
+    await axios.delete(`http://localhost:3000/products/${id}`);
+    alert("Xoa thanh cong");
+  } catch (error) {
+    console.log(error);
+  }
+};
